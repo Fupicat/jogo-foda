123887bbac5d0f47c731ad1faf6f483c7a5c1569
gabrielc <62511047+gabunbr@users.noreply.github.com>
2021-03-15 14:37:07 -0300

    1.7.7

--- src/Player.gd
+++ src/Player.gd
@@ -6,7 +6,7 @@ var self_limite = false
 var life = 100
 var consciencia = true
 var move = Vector2(0, 0)
-var direcao = 0
+var inv = false
 
 func _process(delta):
     if consciencia:
@@ -14,29 +14,22 @@ func _process(delta):
         
         if Input.is_action_pressed("ui_left"):
             move.x = -SPEED
-            direcao = 270
         elif Input.is_action_pressed("ui_right"):
             move.x = SPEED
-            direcao = 90
         else:
             move.x = 0
-        
         if Input.is_action_pressed("ui_up"):
             move.y = -SPEED
-            direcao = 0
         elif Input.is_action_pressed("ui_down"):
             move.y = SPEED
-            direcao = 180
         else:
             move.y = 0
         
-        move.normalized()
-        
-        $Sprite.rotation = lerp_angle($Sprite.rotation, deg2rad(direcao), 0.5)
+        move.normalized() 
         move = lerp(prev_move, move, 0.3)
         move_and_slide(move)
 
-        $"Sprite/Mão".look_at(get_global_mouse_position())
+        look_at(get_global_mouse_position())
 
 func _input(event):
     if event.is_action_pressed("ui_accept") && self_limite == false:
@@ -53,14 +46,18 @@ func _on_Limite_mouse_exited() -> void:
     self_limite = false
 
 func dano(quanto_de_dano, fonte_do_dano):
-    if life > 0:
+    if life > 0 && !inv:
         knockback(fonte_do_dano)
         life -= quanto_de_dano
+        $Timer.start()
+        $CanvasLayer/UI/Text.text = str(life)
         if life <= 0:
             print("game over")
             consciencia = false
             modulate = Color(0, 0, 0)
             $Col.queue_free()
+    if inv:
+        knockback(fonte_do_dano)
 
 func knockback(dano_do_fonte):
     var fonte = dano_do_fonte.position
@@ -68,3 +65,9 @@ func knockback(dano_do_fonte):
     look_at(fonte)
     move = Vector2(-5000, 0).rotated(rotation)
     rotation = prev_dir
+    inv = true
+    $Sprite.modulate = Color(0.431373, 0.431373, 0.431373, 0.647059)
+
+func _on_Timer_timeout() -> void:
+    inv = false
+    $Sprite.modulate = Color(1, 1, 1)
--- src/Tiro.gd
+++ src/Tiro.gd
@@ -3,7 +3,7 @@ extends Area2D
 const SPEED = 500
 
 func _ready():
-    position = $"../Player/Sprite/Mão/Posicao".global_position
+    position = $"../Player/Sprite/Posicao".global_position
     look_at(get_global_mouse_position())
 
 func _process(delta):
