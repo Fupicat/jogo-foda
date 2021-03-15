388e06421f2adae722309d0ca9a1ae0da8f9da25
gabrielc <62511047+gabunbr@users.noreply.github.com>
2021-03-15 17:14:56 -0300

    Tentativa fracassada
    de fazer randomizer :V

--- src/Top Down/scripts/spawnpoints.gd
+++ src/Top Down/scripts/spawnpoints.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
+const INIMIGO = preload("res://Top Down/scenes/inimigo.tscn")
+var posicao = RandomNumberGenerator.new()
+var numero = RandomNumberGenerator.new()
+var node_posicao = 0
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _ready():
+    timer()
 
+func timer():
+    numero.randomize()
+    var tempo_determinado = round(numero.randf_range(1, 3))
+    $Timer.wait_time = tempo_determinado
+    $Timer.start()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#    pass
+func _on_Timer_timeout() -> void:
+    posicao.randomize()
+    var posicao_ini = round(posicao.randf_range(1,4))
+    add_child(INIMIGO.instance())
+    $Inimigo.position = get_node("Position2D_"+str(posicao_ini)).position
+    timer()
