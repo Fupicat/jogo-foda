b36bda34c52c23302b7e153247c1a9e3cbe85c34
Fupi <36973337+Fupicat@users.noreply.github.com>
2021-07-10 18:31:58 -0300

    movimentaçao (em breve state machine)

--- src/Top Down/scripts/Fases/spawnpoints.gd
+++ src/Top Down/scripts/Fases/spawnpoints.gd
@@ -14,32 +14,32 @@ signal wave
 
 func _ready():
 # warning-ignore:return_value_discarded
-    connect("wave", UI, "_acionar_wave")
-    timer()
+	connect("wave", UI, "_acionar_wave")
+	timer()
 
 func timer():
-    randomize()
-    var tempo_determinado = round(rand_range(1, 3))
-    $Timer.wait_time = tempo_determinado
-    $Timer.start()
+	randomize()
+	var tempo_determinado = round(rand_range(1, 3))
+	$Timer.wait_time = tempo_determinado
+	$Timer.start()
 
 func _on_Timer_timeout() -> void:
-    var instancia = INIMIGO.instance()
-    if inimigos != LIMITE && !inimigos_max :
-        inimigos += 1
-        pos = posicoes[round(rand_range(0,3))].position
-        $"..".add_child(instancia)
-        instancia.position = pos
-        timer()
-    elif inimigos >= LIMITE:
-        inimigos_max = true
+	var instancia = INIMIGO.instance()
+	if inimigos != LIMITE && !inimigos_max :
+		inimigos += 1
+		pos = posicoes[round(rand_range(0,3))].position
+		$"..".add_child(instancia)
+		instancia.position = pos
+		timer()
+	elif inimigos >= LIMITE:
+		inimigos_max = true
 
 func _on_Inimigo_death() -> void:
-    kills += 1
-    UI.update_kills(kills)
-    if kills >= LIMITE:
-        inimigos = 0
-        kills = 0
-        inimigos_max = false
-        emit_signal("wave")
-        timer()
+	kills += 1
+	UI.update_kills(kills)
+	if kills >= LIMITE:
+		inimigos = 0
+		kills = 0
+		inimigos_max = false
+		emit_signal("wave")
+		timer()
